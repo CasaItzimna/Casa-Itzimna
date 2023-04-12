6d99028899c8d8a4a9b21d7344d727fe86ca99c5
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2023-04-11 19:55:19 -0600

    Update Reservacion.jsx

--- src/pages/Reservacion.jsx
+++ src/pages/Reservacion.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import {AiOutlineCaretDown} from 'react-icons/ai'
 
 import Calendario from "@/components/Reservacion/Calendario";
+import { differenceInDays } from "date-fns";
 
 function Reservacion() {
 
@@ -33,52 +34,93 @@ function Reservacion() {
         phone:'',
         email:'',
         people:'',
-        begin:'',
-        end: '',
+        
         comments:'',
-        total:''
     })
   }
+  const today = new Date();
 
   const [inicio, setInicio] = useState(null);
   const [fin, setFin] = useState(null);
+  //TODO vamos a crear 2 usestate primer y ultimo dia, el valor inicial es hoy+1 y el ultimo dia hoy+3
+  //depsues vamos a ponerlo en los recuadros de abajo 
   const [show, setShow] = useState(false)
   console.log(show)
+  
+  const monthNames = [
+    "Ene",
+    "Feb",
+    "Mar",
+    "Abr",
+    "May",
+    "Jun",
+    "Jul",
+    "Ago",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dic",
+  ];
+  const precio = 200
+  
+  var totalAux = differenceInDays(inicio,fin)*precio
 
   return (
     <div className="flex flex-col h-full w-full">
       <form className="flex flex-col h-full w-full" onSubmit={handleSubmit}>
-        <label htmlFor="name" className=" text-sm mt-4 ">Name</label>
+        <label htmlFor="name" className=" text-sm mt-4 text-center lg:text-left ">Name</label>
       <input
         type="text"
         name="name"
         id="name"
-        className="border-2 mt-2"
+        className="border-2 mt-2 text-center lg:text-left"
         placeholder="Name"
         onChange={handleInputChange}
         value={formData.name}
         required
       />
-      <label htmlFor="name" className=" text-sm mt-4 ">Phone</label>
+      <label htmlFor="name" className=" text-sm mt-4 text-center lg:text-left ">Phone</label>
       <input
         type="tel"
         name="phone"
         id="phone"
-        className="border-2 mt-2"
+        className="border-2 mt-2 text-center lg:text-left "
         placeholder="Phone"
         onChange={handleInputChange}
         value={formData.phone}
         required
       />
-      <label htmlFor="name" className=" text-sm mt-4 ">Email</label>
+      <label htmlFor="name" className=" text-sm mt-4 text-center lg:text-left ">Email</label>
       <input
         type="email"
         name="email"
         id="email"
-        className="border-2 mt-2"
+        className="border-2 mt-2 text-center lg:text-left"
         placeholder="Email"
         onChange={handleInputChange}
         value={formData.email}
+        required
+      />
+      <label htmlFor="people" className=" text-sm mt-4 text-center lg:text-left ">Personas</label>
+      <input
+        type="number"
+        name="people"
+        id="people"
+        className="border-2 mt-2 text-center lg:text-left"
+        placeholder=""
+        onChange={handleInputChange}
+        value={formData.people}
+        required
+      />
+      <label htmlFor="comments" className=" text-sm mt-4 text-center lg:text-left ">Comentarios</label>
+      <textarea
+        type="text-area"
+        name="coomments"
+        id="comments"
+        className="border-2 mt-2 text-center lg:text-left"
+        placeholder="Comentarios..."
+        onChange={handleInputChange}
+        value={formData.comments}
         required
       />
       <div className="grid grid-cols-2 mt-2 mb-4 px-4 w-full h-full">
@@ -86,23 +128,51 @@ function Reservacion() {
         onClick={()=>setShow(true)}
         >
           <p>Entrada</p>
-          <p className="text-6xl">07</p>
-          <p>abril, 2023</p>
-          <p className="font-thin">Viernes</p>
+          {
+            !inicio?
+            <div>
+
+          <p className="text-6xl">{today.getDate()+1}</p>
+          <p className="mt-2">{monthNames[today.getMonth()]}, {today.getFullYear()}</p>
           <div>
             <AiOutlineCaretDown className="absolute top-[50%] right-[10%]"/>
           </div>
+            </div>
+          :
+          <div>
+
+          <p className="text-6xl">{inicio.getDate()}</p>
+          <p className="mt-2">{monthNames[inicio.getMonth()]}, {inicio.getFullYear()}</p>
+          <div>
+            <AiOutlineCaretDown className="absolute top-[50%] right-[10%]"/>
+          </div>
+            </div>
+          }
         </div>
         <div className="w-1/2 mx-[25%] bg-black text-white flex flex-col text-center relative cursor-pointer"
         onClick={()=>setShow(true)}
         >
           <p>Salida</p>
-          <p className="text-6xl">09</p>
-          <p>abril, 2023</p>
-          <p className="font-thin">Domingo</p>
+          {
+            !fin?
+            <div>
+          <p className="text-6xl">{today.getDate()+3}</p>
+          <p className="mt-2">{monthNames[today.getMonth()]}, {today.getFullYear()}</p>
+          
           <div>
             <AiOutlineCaretDown className="absolute top-[50%] right-[10%]"/>
           </div>
+          </div>
+          :
+          <div>
+
+          <p className="text-6xl">{fin.getDate()}</p>
+          <p className="mt-2">{monthNames[fin.getMonth()]}, {fin.getFullYear()}</p>
+          <div>
+            <AiOutlineCaretDown className="absolute top-[50%] right-[10%]"/>
+          </div>
+            </div>
+          }
         </div>
       </div>
       
@@ -116,6 +186,9 @@ function Reservacion() {
         setShow = {setShow}
         
       />
+    </div>
+    <div className="flex flex-row justify-end">
+      <p>Total: $</p>
     </div>
       <button
           className="w-full bg-black text-white text-sm uppercase font-semibold py-4 mt-4"
