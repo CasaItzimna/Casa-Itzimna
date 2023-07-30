19461dc345a647403c6b7f87c21dbcd6900a67d6
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2023-07-30 05:31:18 -0600

    Update ProductoModal.jsx

--- src/components/Boutique/Grid/ProductoModal.jsx
+++ src/components/Boutique/Grid/ProductoModal.jsx
@@ -53,7 +53,10 @@ localStorage.setItem("producto",JSON.stringify([...carritoProductos, productoCon
       className="modal-overlay"
       overlayClassName="modal-overlay"
     >
-      <div className="w-full h-full flex flex-col justify-center">
+      {
+        producto?
+        <div>
+<div className="w-full h-full flex flex-col justify-center">
         <div className="flex flex-col place-items-center lg:place-content-start lg:flex-row">
           {/* Columna izquierda */}
           <div className="flex flex-col lg:w-[500px] p-4">
@@ -186,6 +189,11 @@ localStorage.setItem("producto",JSON.stringify([...carritoProductos, productoCon
         <Image src={cerrar} alt="cerrar boton " className="w-[80%] lg:w-[30px] h-[80%] lg:h-[30px]" />
         </div>
       </button>
+      </div>
+      :
+      null
+      }
+      
     </Modal>
   );
 }
