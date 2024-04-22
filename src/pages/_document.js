94a14f4cf09cd766437732d42efe4fc0075daaa4
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-04-22 17:25:28 -0600

    Update Google Tag Manager code in _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,10 +7,33 @@ export default function Document() {
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
 <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600&display=swap" rel="stylesheet"/>
+
+ {/* Google Tag Manager */}
+ <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MWG59P8S');`,
+            }}
+          />
+          {/* End Google Tag Manager */}
   
    </Head>
    <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.0.1/model-viewer.min.js" async></script>
       <body>
+
+         {/* Google Tag Manager (noscript) */}
+         <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MWG59P8S"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          {/* End Google Tag Manager (noscript) */}
+         
+
         <Main />
         <NextScript />
       </body>
